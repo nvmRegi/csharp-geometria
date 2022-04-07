7c8c469672196d015d4f4a36b5e25149065b0c62
regi <cabael.regine@gmail.com>
2022-04-07 14:36:06 +0200

    stampaRettangolo

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Geometria;
+
+Console.WriteLine("BENVENUTO NEL MIO PROGRAMMA!\n");
+
+Console.WriteLine("Inserire la base del rettangolo: ");
+int larghezza = Convert.ToInt32(Console.ReadLine()); //larghezza = base
+Console.WriteLine("Inserire l'altezza del rettangolo: ");
+int altezza = Convert.ToInt32(Console.ReadLine());
+
+Rettangolo rettangolo = new Rettangolo(larghezza, altezza);
+
+rettangolo.stampaRettangolo();
---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    internal class Rettangolo
+    {
+        private int baseRettangolo; //attributi
+        private int altezzaRettangolo;
+
+        public Rettangolo(int x, int y) // x = base, y = altezza
+        { //costruttore
+            this.baseRettangolo = x;
+            this.altezzaRettangolo = y;
+        }
+
+        public int calcolaArea() //metodo per calcolare l'area
+        {
+            return baseRettangolo * altezzaRettangolo;
+        }
+
+        public int calcolaPerimetro() //metodo per calcolare il perimetro
+        {
+            return (baseRettangolo + altezzaRettangolo) * 2;
+        }
+
+        public void stampaRettangolo()
+        {
+            Console.WriteLine("—— Rettangolo1 ——");
+            Console.WriteLine("Base: " + baseRettangolo + "cm");
+            Console.WriteLine("Altezza: " + altezzaRettangolo + "cm");
+            Console.WriteLine("Perimetro: " + calcolaPerimetro() + "cm");
+            Console.WriteLine("Area: " + calcolaArea() + "cm");
+            Console.WriteLine("-----------------");
+        }
+    }
+}
